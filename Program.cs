4d0d10555fa5ae558d7bba8f1fec6f8afc684e96
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-04-05 11:27:50 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TESTEIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int f;
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Determinando se um número é par ou ímpar");
+            Console.WriteLine("----------------------------------------");
+            Console.ResetColor();
+            Console.Write("Digite um número: ");
+            Console.ForegroundColor=ConsoleColor.Green;
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.ResetColor();
+            f=(num%2);
+            if (f==1){
+                Console.ForegroundColor=ConsoleColor.Red;
+                  Console.WriteLine("O número informado é ímpar.");
+            } else{
+                Console.ForegroundColor=ConsoleColor.Yellow;
+                Console.WriteLine("O número informado é par.");
+            }
+            Console.ResetColor();
+            Console.Beep();
+        }
+    }
+}
